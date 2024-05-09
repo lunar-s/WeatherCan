21865b695254fa25e1332e34208b6410c281e0a4
Thong Pham <tpham73@my.bcit.ca>
2024-05-09 11:54:45 -0700

    add some code for weathercondition check

--- middleware/weather.js
+++ middleware/weather.js
@@ -2,6 +2,14 @@ const fs = require("fs");
 const https = require("https");
 const cityFinder = require("../controller/cityFinder");
 const parseString = require("xml2js").parseString;
+const weatherConditions = {
+  sunny: ["Sunny", "Mainly Sunny"],
+  suncloud: ["Partly Cloudy", "Mostly Cloudy"],
+  cloudy: ["Haze", "Fog", "Ice Fog", "Fog Patches", "Shallow Fog", "Mist", "Cloudy"],
+  rain: ["Light Rain Shower", "Light Rain Shower and Flurries", "Thunderstorm with Rain", "Thunderstorm", "Heavy Thunderstorm with Rain", "Heavy Thunderstorm", "Light Drizzle", "Heavy Drizzle", "Drizzle", "Light Rain and Drizzle", "Light Rain", "Rain and Drizzle", "Rain Shower", "Heavy Rain and Drizzle", "Heavy Rain Shower", "Heavy Rain", "Rain", "Light Freezing Drizzle", "Light Freezing Rain", "Heavy Freezing Drizzle", "Heavy Freezing Rain", "Light Rain and Snow", "Heavy Rain Shower and Flurries", "Thunderstorm with Heavy Rain",  "Thunderstorm with Light Rain", "Freezing Drizzle", "Freezing Rain"],
+  snow: ["Heavy Rain and Snow", "Light Snow", "Snow", "Flurries", "Heavy Flurries", "Heavy Snow", "Rain and Flurries", "Rain and Snow", "Light Flurries"]
+}
+console.log(weatherConditions['sunny']);
 
 const unitConvert = (temperature) => {
   let temperatureF = temperature * 1.8 + 32;
@@ -35,12 +43,18 @@ const temperatureExtract = (req, res) => {
             result.siteData.currentConditions[0].temperature[0]["_"];
           const currentConditions =
             result.siteData.currentConditions[0].condition;
+            console.log(currentConditions)
           if (unitType === "fahrenheit") {
             res.render("city", {
               city: cityName,
               province: province,
               temperature: `${unitConvert(temperature)} °F`,
               currentConditions: currentConditions,
+              conditionSunny: weatherConditions['sunny'],
+              conditionSuncloud: weatherConditions['suncloud'],
+              conditionRain: weatherConditions['rain'],
+              conditionSnow: weatherConditions['snow'],
+              conditionCloudy: weatherConditions['cloudy'],
             });
           } else {
             res.render("city", {
@@ -48,6 +62,11 @@ const temperatureExtract = (req, res) => {
               province: province,
               temperature: `${temperature} °C`,
               currentConditions: currentConditions,
+              conditionSunny: weatherConditions['sunny'],
+              conditionSuncloud: weatherConditions['suncloud'],
+              conditionRain: weatherConditions['rain'],
+              conditionSnow: weatherConditions['snow'],
+              conditionCloudy: weatherConditions['cloudy'],
             });
           }
         });
