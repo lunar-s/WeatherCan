9626c68b9fb73b448958769c7ccdf4a73f69f0bf
lunar-s <156137303+lunar-s@users.noreply.github.com>
2024-05-10 10:55:25 -0700

    chore: export variables for organization

--- middleware/weather.js
+++ middleware/weather.js
@@ -3,6 +3,7 @@ const https = require("https");
 const cityFinder = require("../controller/cityFinder");
 const parseString = require("xml2js").parseString;
 const weatherConditions = require("./conditions");
+const forecastData = require("../controller/forecast");
 
 const unitConvert = (temperature) => {
   let temperatureF = temperature * 1.8 + 32;
@@ -35,47 +36,44 @@ const temperatureExtract = (req, res) => {
     .replace(" ", "")
     .replace(".", "");
   const city = cityFinder(cityToFind);
+
   if (city) {
     const province = city.properties["Province Codes"];
     const cityName = city.properties["English Names"];
     const cityCode = city.properties.Codes;
     const unitType = req.query.unit;
     const url = `https://dd.weather.gc.ca/citypage_weather/xml/${province}/${cityCode}_e.xml`;
+
     https.get(url, (xml) => {
       let data = "";
       xml.on("data", (chunk) => {
         data += chunk;
       });
-
       xml.on("end", () => {
         parseString(data, (err, result) => {
           if (err) throw err;
-          const temperature =
-            result.siteData.currentConditions[0].temperature[0]["_"];
-          const currentConditions =
-            result.siteData.currentConditions[0].condition;
+          const forecast = forecastData(result);
+
           if (unitType === "fahrenheit") {
             res.render("city", {
               city: cityName,
               province: province,
-              temperature: `${unitConvert(temperature)} °F`,
-              currentConditions: currentConditions,
-              iconCondition: iconCondition(currentConditions),
+              temperature: `${unitConvert(forecast.temperature)} °F`,
+              currentConditions: forecast.currentConditions,
+              iconCondition: iconCondition(forecast.currentConditions),
             });
           } else {
             res.render("city", {
               city: cityName,
               province: province,
-              temperature: `${temperature} °C`,
-              currentConditions: currentConditions,
-              iconCondition: iconCondition(currentConditions),
-            
+              temperature: `${forecast.temperature} °C`,
+              currentConditions: forecast.currentConditions,
+              iconCondition: iconCondition(forecast.currentConditions),
             });
           }
         });
       });
     });
-    // console.log(result.siteData.currentConditions[0].temperature[0]["_"]);
   } else {
     res.render("404", {
       city: req.query.city,
