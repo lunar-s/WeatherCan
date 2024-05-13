ec286e32796db5e4690e4857270d0adbc3ebf239
Quistinie <148367798+quistinie@users.noreply.github.com>
2024-05-13 15:33:48 -0700

    test: update first test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../index.js');
 const chai = require('chai');
 const expect = chai.expect;
+const mocha = require('mocha')
 
 describe('GET /', () => {
     it('responds with 200 status code', (done) => {
