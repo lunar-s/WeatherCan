74a98d70076bd8e723376757e5582b23119c8d84
Thong Pham <tpham73@my.bcit.ca>
2024-05-15 15:25:36 -0700

    test: add forecast data test

--- test/forecastTest.js
+++ test/forecastTest.js
@@ -1,16 +1,25 @@
 var server = require('supertest');
 var should = require('chai').should();
-var weather = require('../controller/forecast').iconCondition;
+var forecast = require('../controller/forecast');
 var app = require("../index");
+var testData = require("../controller/test_data.json");
 
 
 describe("Icon", function() {
     it("should return sunny icon path", function() {
         var condition = "Sunny";
-        var result = weather(condition);
+        var result = forecast.iconCondition(condition);
         should.equal(result, "weather-icons/sunny-icon.png");
-    })
-})
+    });
+});
+
+describe("Forecast Data", function() {
+    it("should take forecast data of a city and return object with 22 keys", function() {
+        var data = testData;
+        var result = forecast.forecastData(data);
+        should.equal(Object.keys(result).length, 22);
+    });
+});
 
 // describe("Index", function() {
 //     it("should pass", function(done){
